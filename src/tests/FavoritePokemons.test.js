fb86dc5967e3bbb6b8708a487646decc5efc38f7
bruce <brucealberto2@gmail.com>
2022-01-28 18:18:13 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('Testar Componente "<FavoritePokemons.js />"', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const msgTitle = screen.getByText(/No favorite pokemon found/i);
+    expect(msgTitle).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonCard = screen.getAllByText(/Pokemon/i);
+    expect(pokemonCard).toHaveLength(1);
+  });
+});

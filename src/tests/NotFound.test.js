b8b5162e41d2be1245e1e9a82e6c19b65fae9543
bruce <brucealberto2@gmail.com>
2022-01-28 19:00:15 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import App from '../App';
+
+describe('Testar o componente "<NotFound/>"', () => {
+  it('Teste se página contém um "h2" com o texto "Page requested not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/teste');
+    const title = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested/i,
+    });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(/Page requested not found/i);
+  });
+
+  it('Teste se página mostra a imagem ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/teste');
+    const image = screen.getByAltText(
+      /Pikachu crying because the page requested was not found/i,
+    );
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
